020e1fc82c78d7442a8d113e8b0396f11ffaf73c
svijayk29 <vijay.roark@gmail.com>
2018-03-01 11:17:22 -0800

    Add node app

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var request = require('supertest');
+var app = require('../app.js');
+ 
+describe('GET /', function() {
+	  it('respond with vijay hello to you', function(done) {
+		      request(app).get('/').expect('vijay hello to you', done);
+		    });
+});
